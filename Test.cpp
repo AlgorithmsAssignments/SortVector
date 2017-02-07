af1961c399f0085cbc2268a856cd0f714aa9e399
Turupawn <ahmed.hn.43@gmail.com>
2017-02-07 17:39:36 -0600

    first commit

---
+++ Test.cpp
@@ -0,0 +1,26 @@
+#include "Test.h"
+
+void test()
+{
+    vector<int> v1;
+    for(int i=0;i<10;i++)
+    {
+        v1.push_back(rand()%100);
+        cout<<v1[i]<<endl;
+    }
+
+    bool is_sorted = true;
+
+    for(int i=1;i<v1.size();i++)
+        if(v1[i]<v1[i-1])
+            is_sorted = false;
+
+    if(is_sorted)
+    {
+        cout<<"Test: Pass"<<endl;
+    }else
+    {
+        cout<<"Test: Fail"<<endl;
+    }
+
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+vector<int> sort(vector<int> numbers)
+{
+    vector<int> answer;
+    return answer;
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
